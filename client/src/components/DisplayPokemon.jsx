687045c81bd5086e51cfa528e9a23903bf20d2e8
FahmiAlmsyah <pahmialamsya@gmail.com>
2025-02-07 14:35:47 +0700

    resolve

--- client/src/components/DisplayPokemon.jsx
+++ client/src/components/DisplayPokemon.jsx
@@ -19,9 +19,9 @@ export default function DisplayPokemon({pokemonData}) {
                 { Array.isArray(pokemonData) && pokemonData.map((item, index) => (
                 <div key={item.name} className="bg-white rounded-2xl shadow-md p-4 flex justify-between items-center">
                     <div>
-                        <h2 className="text-xl font-bold">{item.name}</h2>
+                        <h2 className="text-xl font-bold capitalize">{item.name}</h2>
                         <p className="text-gray-600 text-sm mb-2">#{String(index + 1).padStart(4, '0')}</p>
-                        <p className="text-gray-600 italic text-sm">{item.type}</p>
+                        <p className="text-gray-600 italic text-sm capitalize">{item.type}</p>
                         <div className="flex space-x-2 mt-2">
                                 {item.color.split(', ').map((color, index) => (
                                     <div key={index} className="w-4 h-4 rounded-full" style={{ backgroundColor: color }}></div>
